3736879aa2a17da7d6cd3c1dc5549f326316cb56
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-05-11 11:37:58 +0530

    added files for and code for deboucing with 2 usecase

--- debouncing.js
+++ debouncing.js
@@ -7,14 +7,36 @@ function fetch() {
 
 //debounce func
 function debounce(cb, delay) {
-  let timer;
+  var timer;
   return function (...args) {
     if (timer) clearTimeout(timer);
-    else setTimeout(cb, delay);
+    timer = setTimeout(() => {
+      cb(...args);
+    }, delay);
   };
 }
 
-let optimisedFunc = debounce(fetch, 1000);
-// console.log(typeof optimisedFunc);
-// optimisedFunc();
-// fetch();
+let optimisedFunc = debounce(fetch, 100);
+
+// Debouncing for button
+let btn = document.getElementById("btn");
+let total = 0;
+
+function debouncer(cb, delay) {
+  var timer;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
+
+btn.addEventListener(
+  "click",
+  debouncer(() => {
+    alert("Payment Successfull");
+    // console.log("clicked", total++);
+  }, 1000)
+);
