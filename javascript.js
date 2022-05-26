4172a0b6425bf1a6819d0b57eb82cac897b70f93
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-05-26 21:41:53 +0530

    added react and js interview questions

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// Javascript Interview Questions
+
+// what is Javascript ??
+// Different types of datatype??
+// Hoisting (functional hoisting and variable hoisting)
+// How inheritance is implemented in javascript ??
+// difference betw map, forEach and filter
+// What is coercion or type coercion ?? Implicit and Explitcit with Example??
+// Async Events are Handled in JS
+// Difference between == && ===
+// Promises with many Question ??
+// WHat is the need to use promise
+// How to run multiple promises together ??
+// How to stop resolving multiple promise if any one them is fullfilled ??
+// Advantages and disadvantages of promise.all()
+// promise.all(), promise.any(), promise.race() etc...
+// Closures in Javascript ?? Advantages ?? Problems with CLosures ?? Real Code ??
+
+// Event Bubbling and Event Capturing and How to stop it?? stopPropagation() and stopImmediatePropagtion ??
+// Deep Copy in Javascript ??
+// Differnce between localStorage, session and cookies ??
+// What is scope chainging ??
+// difference betw null, undefined and not defined ??
+// What is lexical this ??
+// stuff related to functions
+//Anonymous function
+//Function Expression
+//Function Constructor
+//IIFE
+//First class Function
+//Higher Order Function
+//Call Back Function
+//ES6 / Typescript
+//some features of ES6 and some of them
+// Function Constructor and Arrow Function
+// Spread and Rest and Code with Example
+// var, let, const
+// Destrucing and useCase with example
+// Default values in ES6
+// Array method and String methods
+
+//Difference between prototype and __proto__ ??
+//What is polyfull ?? How to create a polyfill for bind() and promise.all() etc...
+//Debouncing and its benefits and code
+//Throttling and code
+//differnce between then/catch and async/await ??
+//what is memoization and code for memoized function ??
+//What is currying and implement infinite currying with code ??
+//differnce between for and forEach ??
+//mutable and immutable objects ??
+//let and const hoisted and temporaral dead zone ??
+
+// ****** OUTPUT BASED QUESTION ******
+//output based question on setTimeout
+// promise and setTimeout
+// this in arrow and normal func
+// type coercion "2" + 2, "2" - 2
+// OBJECT properties and ways to delete properties in object
+// var a = b = 3;
---
+++ react.js
@@ -0,0 +1,41 @@
+//  React Inteview Questions
+
+// 1. What is Pure Component (Advantages and Disadvantages)
+
+// 2. What is UseEffect Hook
+// * How can we call it only once
+// * What Happens if we dont pass the dependency array
+// * How to do cleanUp in useEffect or How to use it like ComponentDidUnmount
+// * How to call it on the basis of specific parameter
+
+// 3. Why Functional Components are better??
+// 4. How to pass data from child component to parent component in react??
+// 5. Different ways to add styling in React components??
+// 6. Which is better choice for React state management - CONTEXT API or REDUX and Why ??
+// 7. What is React.Memo and Why it is used and How memoization is done in React ??
+// 8. What is React Router DOM??
+
+// What is React ?? How it is fast ?? framework and react difference ?? Why single page application
+// Internal working of React ?? VDom ?? Shallow Dom
+// Explain React LifeCycle ??Order of all lifecycle methods ?? How it works in functional components ??
+// getDerivedStateFromProps() || shouldComponentUpdate() || getSnapshotBeforeUpdate() etc...
+// What are Synthetic Events in React ? Why they were Introduced ??
+// Difference between State and Props ??
+// Stateful and Stateless Components
+// What is JSX
+// What is React Fragments and why do we use it??
+// Why do we use key in fragments ??
+
+//What are Higher Components ?? Why do we use it ?? Example code
+// Advantages and Disadvantages || fix for HOC
+
+// What is Prop drilling??
+// What is State Lifting in React ?? Give some practical scenario to use that ??
+// What is React Hooks ?? Different types and why it is introduced ??
+// What are the Hooks that you have used ??
+// Custom Hook our own Hook and in Which scenario ??
+// What are Controlled and Uncontrolled components ?? Give Examples ??
+// What is setState() ?? how it works ?? setState() is Synchronous or Asynchoronous ?? What is the use of callback function inside the setState()?
+// Should we call setState in Render Method ??
+// What is the use of key in React ?? can we use index as key ?? In which situation we not use index as keys ?? Disadvantages of using index as a key ??
+// How to validate Props in React ??
